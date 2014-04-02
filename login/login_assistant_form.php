e47e99cb449ca95b17c43ee066c5ce6616fe4f38
Simon Trendell <Simon.Trendell@hts.army.mod.uk>
2014-04-02 15:01:49 +0100

    Improved validation

--- login/login_assistant_form.php
+++ login/login_assistant_form.php
@@ -13,6 +13,7 @@ class login_assistant_form_step1 extends moodleform {
 
       $mform->addElement('password', 'letterpassword', 'Password');
       $mform->setType('letterpassword', PARAM_TEXT);
+      $mform->addRule('letterpassword', 'Please provide the password on your course acknowledgement letter.', 'required', null, 'client');
       
       $mform->addElement('hidden', 'step', 'step2');
       $mform->setType('step', PARAM_ACTION);
@@ -31,6 +32,7 @@ class login_assistant_form_step2 extends moodleform {
       $mform->setDisableShortforms(true);
       
       $mform->addElement('date_selector', 'course_start_date', 'Course Start Date');
+      $mform->addRule('course_start_date', 'Please provide the password on your course acknowledgement letter.', 'required', null, 'client');
       
       $mform->addElement('hidden', 'step', 'step3');
       $mform->setType('step', PARAM_ACTION);
@@ -50,9 +52,13 @@ class login_assistant_form_step3 extends moodleform {
       
       $mform->addElement('text', 'servicenumber', 'Service Number');
       $mform->setType('servicenumber', PARAM_TEXT);
+      $mform->addRule('servicenumber', 'Please enter your service number', 'required', null, 'client');
+      $mform->addRule('servicenumber', 'Please enter your service number', 'alphanumeric', null, 'client');
       
       $mform->addElement('text', 'surname', 'Surname');
       $mform->setType('surname', PARAM_TEXT);
+      $mform->addRule('surname', 'Please enter your full surname, do not include first or middle names', 'required', null, 'client');
+      $mform->addRule('surname', 'Please enter your full surname', 'lettersonly', null, 'client');
       
       $mform->addElement('hidden', 'step', 'step4');
       $mform->setType('step', PARAM_ACTION);
